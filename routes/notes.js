b59f5b5ad143afc8e6454271cb6faec062b3c072
adityan-dhristhi <adityan@dhristhi.com>
2025-02-11 15:10:10 +0530

    adding note route

--- routes/notes.js
+++ routes/notes.js
@@ -4,4 +4,8 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.send("Notes Route");
 });
+
+router.get("/:id", (res,req)=>{
+  res.send(req.params.id);
+})
 module.exports = router;
