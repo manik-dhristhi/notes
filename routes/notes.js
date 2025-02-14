db902a1ca65579f62f77c7c8276c862f897d332d
manik-dhristhi <manikm@dhristhi.com>
2025-02-14 10:33:09 +0530

    Update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { createNotes, getNotes, getNotesById } = require("../controllers/Notes");
 
 router.get("/", (req, res) => {
-  res.send("Notes Route");
+  res.send("User Route");
 });
+router.post("/create", createNotes);
+router.get("/getnote", getNotes);
+router.get("/getnote/:id", getNotesById);
 module.exports = router;
